2287fae420e314f4dd8dd79b894bd977bc7a5859
OlegBon <oleg_bon@ukr.net>
2024-11-21 16:39:14 +0200

    fix key (TodoList.jsx) and checked -> defaultChecked (Task.jsx)

--- src/Components/TodoList/Task/Task.jsx
+++ src/Components/TodoList/Task/Task.jsx
@@ -1,13 +1,13 @@
-import './Task.css';
+import "./Task.css";
 
-const Task = ({task}) => {
-    return (
-        <div className="Task">
-            <input type="checkbox" checked={task.completed}/>
-            <p>{task.text}</p>
-            <button>Remove</button>
-        </div>
-    );
-}
+const Task = ({ task }) => {
+  return (
+    <div className="Task">
+      <input type="checkbox" defaultChecked={task.completed} />
+      <p>{task.text}</p>
+      <button>Remove</button>
+    </div>
+  );
+};
 
 export default Task;
--- src/Components/TodoList/TodoList.jsx
+++ src/Components/TodoList/TodoList.jsx
@@ -1,34 +1,33 @@
-import './TodoList.css';
-import {useDispatch, useSelector} from "react-redux";
+import "./TodoList.css";
+import { useDispatch, useSelector } from "react-redux";
 import Task from "./Task/Task";
-import {addTask, ChangeTaskText} from "../../Redux/TodoListReducer";
+import { addTask, ChangeTaskText } from "../../Redux/TodoListReducer";
 
 const TodoList = () => {
-    const tasks = useSelector((state) => state.tasks);
-    const dispatch = useDispatch();
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
 
-    const onChangeHandler = (e) => {
-        dispatch(ChangeTaskText(e.target.value));
-    }
+  const onChangeHandler = (e) => {
+    dispatch(ChangeTaskText(e.target.value));
+  };
 
-    const onClickHandler = () => {
-        dispatch(addTask());
-    }
+  const onClickHandler = () => {
+    dispatch(addTask());
+  };
 
-
-    return (
-        <div className="TodoList">
-            <div className="inputPanel">
-                <input type="text" onChange={(e) => onChangeHandler(e)}/>
-                <button onClick={onClickHandler}>Add task</button>
-            </div>
-            <div>
-                {
-                    tasks.map(task => <Task task={task} />)
-                }
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="TodoList">
+      <div className="inputPanel">
+        <input type="text" onChange={(e) => onChangeHandler(e)} />
+        <button onClick={onClickHandler}>Add task</button>
+      </div>
+      <div>
+        {tasks.map((task, idx) => (
+          <Task task={task} key={idx} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TodoList;
